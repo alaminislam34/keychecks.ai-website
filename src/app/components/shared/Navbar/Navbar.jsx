57ce4978a84c2bc7237dbcffe4a3255406feb8b7
alaminislam34 <mi3548514@gmail.com>
2025-12-14 01:22:50 +0600

    design navbar component

--- src/app/components/shared/Navbar/Navbar.jsx
+++ src/app/components/shared/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export const navlinks = [
   {
     href: "/",
@@ -24,13 +27,55 @@ export const navlinks = [
 ];
 
 const Navbar = () => {
+  const path = usePathname();
   return (
-    <div>
-      <h1>
-        <span className="text-primary">K</span> KeyChecks.
-        <span className="text-primary">ai</span>
-      </h1>
-    </div>
+    <section className="py-4 bg-white fixed w-full top-0 border-b border-gray/40">
+      <nav className="flex justify-between items-center max-w-360 mx-auto w-11/12">
+        {/* logo */}
+        <div className="font-bold tracking-wider text-xl">
+          <h1>
+            <span className="bg-primary text-white py-1 px-3 rounded-lg font-bold mr-2">
+              K
+            </span>
+            KeyChecks.
+            <span className="text-primary">ai</span>
+          </h1>
+        </div>
+
+        <div className="flex items-center justify-between gap-6">
+          {/* link */}
+          <div>
+            <ul className="flex flex-row gap-6 items-center">
+              {navlinks.map((nav, i) => (
+                <li key={i}>
+                  <Link
+                    href={nav.href}
+                    className={`hover:text-primary text-sm font-semibold tracking-wider ${
+                      path === nav.href ? "text-primary" : "text-[#64748B]"
+                    }`}
+                  >
+                    {nav.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* button */}
+          <div className="flex items-center gap-4">
+            <button className="bg-primary border border-primary text-white py-2 px-6 rounded-lg text-sm font-semibold tracking-wider">
+              Get My Report
+            </button>{" "}
+            <Link
+              href={"/login"}
+              className="hover:underline hover:text-primary"
+            >
+              Log in
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </section>
   );
 };
 
